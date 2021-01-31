daa846ebd12dcbdc678effbfe3d1b02c7327574d
yuan <ayrw@163.com>
2021-01-31 22:24:24 +0800

    M

--- extract_tb_data.py
+++ extract_tb_data.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.offsetbox import (TextArea, DrawingArea, OffsetImage,
                                   AnnotationBbox)
-
+from scipy.ndimage import gaussian_filter1d
 logs = [
     {'name': 'tusimple_semi_0.05',
         'path': './experiment_log/semi_tusimple_finished_20iter_0.05'},
@@ -40,6 +40,40 @@ def getLogDataWithMultiStep(logGroupDict, group, cat, name, iter=0, step=0):
 
 def getLogDataSummary(logGroupDict, group, cat, name):
     return logGroupDict[group]['%s_summary/%s' % (cat, name)]
+
+
+def getDataList(logGroupDict, group, cat, name, step, iters):
+    return [getLogDataWithMultiStep(logGroupDict, group, cat, name, iter, step) for iter in iters]
+
+
+def make_plot_no_marker(dataList, labelList, iterEpoch, xlabel, ylabel, title):
+    fig, ax = plt.subplots(figsize=(4, 2.7), constrained_layout=True)
+    alphalist = np.linspace(0.1, 1, len(dataList))
+    for data, label, alpha, linew in zip(dataList, labelList, alphalist, np.flip(alphalist)):
+        data = np.array(data)
+        y_smoothed = gaussian_filter1d(data[:, 1], sigma=0.1)
+        ax.plot(data[:, 0], y_smoothed, color='red',
+                label=label, linewidth=linew*5, alpha=alpha)
+    # plt.yscale("log")
+    # ax.legend()
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+
+    if iterEpoch is not None:
+        iterEpoch = np.array(iterEpoch)
+
+        def iter2Epoch(x):
+            return np.interp(x, iterEpoch[:, 0], iterEpoch[:, 1])
+
+        def epoch2Iter(x):
+            return np.interp(x, iterEpoch[:, 1], iterEpoch[:, 0])
+
+        secax = ax.secondary_xaxis('top', functions=(iter2Epoch, epoch2Iter))
+        secax.set_xlabel('Epoch')
+    plt.savefig(title+'.pdf')
+    plt.show()
 
 
 def make_plot(dataList, labelList, iterEpoch, xlabel, ylabel, title):
@@ -118,3 +152,10 @@ make_plot(dataList2, labalList, None,
 make_plot(dataList3, labalList, None,
           'Iteration', 'FP', 'FP')
 
+epch = getLogDataWithMultiStep(
+    logGroupDict, 'tusimple_semi_0.1', 'meta', 'epoch', 1, 3)
+dl = getDataList(logGroupDict, 'tusimple_semi_0.1',
+                 'metric', 'iou', 3, range(1, 19))
+ll = ['iter %d' % i for i in range(1, 19)]
+make_plot_no_marker(dl, ll, epch,
+                    'Iteration', 'XX', 'XXXX')
